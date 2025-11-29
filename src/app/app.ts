f69cabb41b4f14530d69503754eacee413b19820
valeria <yessicavalemartinez@udetijuana.edu.mx>
2025-11-29 11:40:05 -0800

    segundocambio y crecacion de componentes

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './components/navbar-component/navbar-component';
+import { Components } from './components/components';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent, Components],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ src/app/components/piel-component/piel-component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-piel-component',
+  imports: [],
+  templateUrl: './piel-component.html',
+  styleUrl: './piel-component.css',
+})
+export class PielComponent {
+
+}
