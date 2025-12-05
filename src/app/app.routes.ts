3522a60b8e09bb4977e1ce86ab040167adc22573
valeria <yessicavalemartinez@udetijuana.edu.mx>
2025-12-05 13:22:50 -0800

    imagenes insertadas y diseno implementado

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,9 +3,12 @@ import { NavbarComponent } from './components/navbar-component/navbar-component'
 import { PielComponent } from './components/piel-component/piel-component';
 import { RutinaComponent } from './components/rutina-component/rutina-component';
 import { ResenaComponent } from './components/resena-component/resena-component';
+import { InicioComponent } from './components/inicio-component/inicio-component';
 
 export const routes: Routes = [
+{path: '',component: InicioComponent},
 {path: 'piel-enlace', component: PielComponent},
 {path: 'rutina-enlace', component: RutinaComponent},
-{path: 'resena-enlace', component: ResenaComponent}
+{path: 'resena-enlace', component: ResenaComponent},
+{path: '**', redirectTo: ''},
 ];
