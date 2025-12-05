b8ce5f69b69b3a2cafa2955eee6bf670ad6ceee7
valeria <yessicavalemartinez@udetijuana.edu.mx>
2025-12-04 19:44:34 -0800

    cambios improvisador

---
+++ src/app/components/resena-component/resena-component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-resena-component',
+  imports: [],
+  templateUrl: './resena-component.html',
+  styleUrl: './resena-component.css',
+})
+export class ResenaComponent {
+
+}
