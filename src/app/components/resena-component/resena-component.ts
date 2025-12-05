23309defe5bff90099e1088eb61b640ef1d41caf
valeria <yessicavalemartinez@udetijuana.edu.mx>
2025-12-04 21:18:39 -0800

    sin imagenes ni css

--- src/app/components/resena-component/resena-component.ts
+++ src/app/components/resena-component/resena-component.ts
@@ -1,11 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { Resena } from '../../models/resena-model';
+import { ResenaServicio } from '../../services/resena-servicio';
 
 @Component({
   selector: 'app-resena-component',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './resena-component.html',
-  styleUrl: './resena-component.css',
+  styleUrl: './resena-component.css'
 })
-export class ResenaComponent {
+export class ResenaComponent implements OnInit {
+  resenas: Resena[] = [];
+  
+  nuevaResena: Resena = {
+    nombreProducto: '',
+    puntuacion: 5,
+    descripcion: ''
+  };
 
+  constructor(private resenaServicio: ResenaServicio) {}
+
+  ngOnInit(): void {
+    this.resenas = this.resenaServicio.obtenerResenas();
+  }
+
+  agregarResena(): void {
+    if (this.nuevaResena.nombreProducto && this.nuevaResena.descripcion) {
+      this.resenaServicio.agregarResena(this.nuevaResena);
+      this.resenas = this.resenaServicio.obtenerResenas();
+      
+      // Resetear formulario
+      this.nuevaResena = {
+        nombreProducto: '',
+        puntuacion: 5,
+        descripcion: ''
+      };
+    }
+  }
 }
